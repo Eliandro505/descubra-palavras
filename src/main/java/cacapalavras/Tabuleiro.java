f11a2c70c589c6a993a49253492d124e9d0cf96f
Eliandro <eliandro.gomes42@gmail.com>
2025-04-10 22:25:01 -0300

    Adição do pacoto cacapalavras

---
+++ src/main/java/cacapalavras/Tabuleiro.java
@@ -0,0 +1,15 @@
+package cacapalavras;
+
+import java.util.Random;
+
+public class Tabuleiro {
+    Integer tamanho;
+    Random rand = new Random();
+
+    public Tabuleiro() {
+
+    }
+
+    private criarTabuleiro() {
+    }
+}
