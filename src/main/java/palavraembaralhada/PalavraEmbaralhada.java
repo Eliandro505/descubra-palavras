0b827a1c610f83b4c82111747c268e227aaf7592
Eliandro <eliandro.gomes42@gmail.com>
2025-04-19 05:05:36 -0300

    decomposição do método run da classe PalavraEmbaralhada em métodos privados e atributos de classe

--- src/main/java/palavraembaralhada/PalavraEmbaralhada.java
+++ src/main/java/palavraembaralhada/PalavraEmbaralhada.java
@@ -7,30 +7,47 @@ import util.Pontuacao;
 import java.util.Scanner;
 
 public class PalavraEmbaralhada {
+    private Scanner scan;
+    private Palavras palavra;
+    private Pontuacao pontos;
+    private Dicas dica;
+    private String palpite= "";
+
     public void run() {
-        Palavras palavra = new Palavras(1);
-        Pontuacao pontos = new Pontuacao();
-        Dicas dica = new Dicas(palavra.palavraEscolhida);
+        iniciarJogo();
 
-        System.out.printf("Digite [dica] para receber uma dica. \nDigite [0] para desistir\n");
-
-        Scanner scan = new Scanner(System.in);
-        System.out.printf("[------] ");
         while (true) {
-            System.out.printf("Adivinhe a seguinte palavra: %s // ", palavra.palavraEmbaralhada.toLowerCase());
-            String entrada = scan.nextLine().toLowerCase();
-            if (entrada.equals("0")) break;
-            else if (entrada.equals("dica")) {
-                pontos.removerPontos();
-                dica.mostrarDica();
-                System.out.printf("[------] ");
-            }
-            else if (entrada.equals(palavra.palavraEscolhida.toLowerCase())) {
-                Palavras.printPadronizado("ACERTOU");
-                Palavras.printPadronizado(pontos.retornarPontuacao());
-                break;
-            }
-            else System.out.printf("[ERRADO] ");
+            palpite = lerEntrada();
+            if (palpite.equals("0")) break;
+            boolean acertou = processarPalpite(palpite);
+            if (acertou) break;
         }
+    }
+
+    private void iniciarJogo() {
+        palavra = new Palavras(1);
+        pontos = new Pontuacao();
+        dica = new Dicas(palavra.palavraEscolhida);
+        scan = new Scanner(System.in);
+        System.out.printf("Digite [dica] para receber uma dica. \nDigite [0] para desistir\n");
+        System.out.printf("[------] ");
+    }
+
+    private String lerEntrada() {
+        System.out.printf("Adivinhe a seguinte palavra: %s // ", palavra.palavraEmbaralhada.toLowerCase());
+        return scan.nextLine().toLowerCase();
+    }
+
+    private boolean processarPalpite(String palpite) {
+        if (palpite.equals("dica")) {
+            pontos.removerPontos();
+            dica.mostrarDica();
+            System.out.printf("[------] ");
+        } else if (palpite.equals(palavra.palavraEscolhida.toLowerCase())) {
+            Palavras.printPadronizado("ACERTOU");
+            Palavras.printPadronizado(pontos.retornarPontuacao());
+            return true;
+        } else System.out.printf("[ERRADO] ");
+        return false;
     }
 }
